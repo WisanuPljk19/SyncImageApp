5b3f0e409a5d6de5dc3af747e16fa0b8d8594612
Wisanu <wisanu.pljk@gmail.com>
2021-11-15 15:30:18 +0700

    Initial Commit

---
+++ SynceImageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SynceImageApp
+//
+//  Created by Wisanu Paunglumjeak on 15/11/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
